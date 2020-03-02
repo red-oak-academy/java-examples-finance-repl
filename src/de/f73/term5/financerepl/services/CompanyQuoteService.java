cf46a7c948a68a11b2df7a0da2ce1298d3c079a5
Dean Eckert <dean.eckert@red-oak-consulting.com>
2020-03-02 20:07:48 +0100

    removed

--- src/de/f73/term5/financerepl/services/CompanyQuoteService.java
+++ src/de/f73/term5/financerepl/services/CompanyQuoteService.java
@@ -1,10 +1,6 @@
 package de.f73.term5.financerepl.services;
 
-import de.f73.term5.financerepl.http.HttpRequestUtil;
-import de.f73.term5.financerepl.json.JsonParserUtils;
 import de.f73.term5.financerepl.repl.Command;
-
-import java.io.IOException;
 
 public class CompanyQuoteService implements Command {
 
@@ -24,15 +20,8 @@ public class CompanyQuoteService implements Command {
     public boolean handle(String expression, String[] args) {
         System.out.println("Fetching Company Profile...");
 
-        try {
-            //TODO keep going here
-            String result = HttpRequestUtil.getUrl("https://financialmodelingprep.com/api/v3/company/profile/" + args[0]);
-            System.out.println(result);
-            JsonParserUtils.parseFromString(result);
-        } catch (IOException e) {
-            e.printStackTrace();
-            return false;
-        }
+        //TODO keep going here
+
 
         return true;
     }
